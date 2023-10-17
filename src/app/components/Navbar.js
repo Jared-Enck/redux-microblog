704051cec7d73a6339da0d25b7a0aaf8a998c845
Jared Enck <jaredenck86@gmail.com>
2023-10-17 15:29:55 -0400

    add IconButton to new post link

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { PostAdd } from '@mui/icons-material';
 
 export default function Navbar() {
   return (
@@ -8,11 +9,19 @@ export default function Navbar() {
       position='static'
       sx={{ bgcolor: 'primary.main' }}
     >
-      <Toolbar>
+      <Toolbar sx={{ display: 'flex' }}>
         <Link href={'/'}>
           <Typography sx={{ fontSize: '1.7rem', color: 'primary.text' }}>
             Blog
           </Typography>
+        </Link>
+        <Link
+          href={'/new'}
+          style={{ marginLeft: 'auto' }}
+        >
+          <IconButton>
+            <PostAdd sx={{ color: 'primary.text' }} />
+          </IconButton>
         </Link>
       </Toolbar>
     </AppBar>
