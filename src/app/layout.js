5171577228ff42aa4a05388678f48f03c4539c53
Jared Enck <jaredenck86@gmail.com>
2023-10-17 15:28:27 -0400

    add CssBasline

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 import Navbar from './components/Navbar';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './theme';
+import { CssBaseline } from '@mui/material';
 
 export const metadata = {
   title: 'Redux Microblog',
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body className={roboto.className}>
         <ThemeProvider theme={theme}>
+          <CssBaseline />
           <Navbar />
           <main>{children}</main>
         </ThemeProvider>
