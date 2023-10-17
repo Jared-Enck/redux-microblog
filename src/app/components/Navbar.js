16c36c8e071bdd1ccefd21362a529e88748617e2
Jared Enck <jaredenck86@gmail.com>
2023-10-17 18:14:59 -0400

    add container with maxWidth lg

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import Link from 'next/link';
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Container,
+} from '@mui/material';
 import { PostAdd } from '@mui/icons-material';
 
 export default function Navbar() {
   return (
     <AppBar
+      component={'nav'}
       position='static'
       sx={{ bgcolor: 'primary.main' }}
     >
-      <Toolbar sx={{ display: 'flex' }}>
-        <Link href={'/'}>
-          <Typography sx={{ fontSize: '1.7rem', color: 'primary.text' }}>
-            Blog
-          </Typography>
-        </Link>
-        <Link
-          href={'/new'}
-          style={{ marginLeft: 'auto' }}
-        >
-          <IconButton>
-            <PostAdd sx={{ color: 'primary.text' }} />
-          </IconButton>
-        </Link>
-      </Toolbar>
+      <Container maxWidth='xl'>
+        <Toolbar>
+          <Link href={'/'}>
+            <Typography sx={{ fontSize: '1.7rem', color: 'primary.text' }}>
+              Blog
+            </Typography>
+          </Link>
+          <Link
+            href={'/new'}
+            style={{ marginLeft: 'auto' }}
+          >
+            <IconButton>
+              <PostAdd
+                sx={{ color: 'primary.text', transform: 'scale(1.3)' }}
+              />
+            </IconButton>
+          </Link>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 }
