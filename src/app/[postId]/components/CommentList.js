0d56ce916a6cbfc733e114636830c4430d622be3
Jared Enck <jaredenck86@gmail.com>
2023-10-18 14:35:56 -0400

    add solo borderRadius for CommentItem and msg for no comments

--- src/app/[postId]/components/CommentList.js
+++ src/app/[postId]/components/CommentList.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Stack, Divider, Box, Typography } from '@mui/material';
 import CommentItem from './CommentItem';
 import CommentForm from './CommentForm';
 
-const comments = [
-  { id: '1', text: 'dis a comment' },
-  { id: '2', text: 'another one' },
-  { id: '3', text: 'another one, again' },
-];
-
 export default function CommentList() {
+  const [comments, setComments] = useState([
+    { id: '1', text: 'dis a comment' },
+    { id: '2', text: 'another one' },
+  ]);
+
   return (
     <Stack spacing={2}>
       <Box>
@@ -17,26 +16,48 @@ export default function CommentList() {
         <Divider sx={{ bgcolor: 'primary.light', margin: '1rem' }} />
       </Box>
 
-      <Box>
-        {comments.map((c, idx) => {
-          const first = idx === 0 ? '.3rem .3rem 0rem 0rem' : null;
-          const last =
-            idx === comments.length - 1 ? '0rem 0rem .3rem .3rem' : null;
-          return (
-            <Box
-              key={c.id}
-              sx={{
-                borderRadius: first || last,
-                backgroundColor:
-                  idx % 2 === 1 ? 'primary.light' : 'primary.lightest',
-              }}
-            >
-              <CommentItem comment={c} />
-            </Box>
-          );
-        })}
+      <Box sx={{ height: '30vh', overflow: 'auto' }}>
+        {comments.length ? (
+          comments.map((c, idx) => {
+            const first = idx === 0 ? '.3rem .3rem 0rem 0rem' : null;
+            const last =
+              idx === comments.length - 1 ? '0rem 0rem .3rem .3rem' : null;
+            const solo = comments.length === 1 ? '.3rem' : null;
+            return (
+              <Box
+                key={c.id}
+                sx={{
+                  borderRadius: solo || first || last,
+                  backgroundColor:
+                    idx % 2 === 1 ? 'primary.light' : 'primary.lightest',
+                }}
+              >
+                <CommentItem
+                  setComments={setComments}
+                  comment={c}
+                  idx={idx}
+                />
+              </Box>
+            );
+          })
+        ) : (
+          <Typography
+            variant='h6'
+            sx={{ fontStyle: 'italic', padding: 1 }}
+          >
+            No comments yet...
+          </Typography>
+        )}
       </Box>
-      <CommentForm />
+
+      <Box>
+        <Divider sx={{ bgcolor: 'primary.light', margin: '1rem' }} />
+      </Box>
+
+      <CommentForm
+        currLength={comments.length}
+        setComments={setComments}
+      />
     </Stack>
   );
 }
