2902b89dc3d70db0860f3b98b93f68295198c007
Jared Enck <jaredenck86@gmail.com>
2023-10-19 18:15:19 -0400

    updated for new titleSlice

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,22 @@
 import React, { useEffect } from 'react';
 import { Container, Typography, Divider } from '@mui/material';
 import PostList from './components/PostList';
-import { useSelector, shallowEqual } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { fetchTitles } from '@/redux/reducers/titleSlice';
 
 export default function Home() {
-  const posts = useSelector((store) => store.root.posts, shallowEqual);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTitles());
+  }, [dispatch]);
+
+  const titles = useSelector(
+    (store) => store.root.titleReducer.titles,
+    shallowEqual
+  );
+  const isLoading = useSelector((state) => state.root.titleReducer.isLoading);
+  const error = useSelector((state) => state.root.titleReducer.error);
 
   return (
     <Container maxWidth='lg'>
@@ -19,7 +31,11 @@ export default function Home() {
 
       <Divider sx={{ bgcolor: 'primary.light', marginBottom: '1.5rem' }} />
 
-      <PostList posts={Object.entries(posts)} />
+      <PostList
+        titles={titles}
+        isLoading={isLoading}
+        error={error}
+      />
     </Container>
   );
 }
